c650ac6feaeb37253894202ed873d24569688284
Max_Divizion <99737414+Max-Divizion@users.noreply.github.com>
2024-10-04 15:01:58 +0300

    Add map

---
+++ Assets/Objects/bathroom/Worn_Bathroom/Readme.txt
@@ -0,0 +1,27 @@
+This models contains Unreal Engine 4 collision models. If you don't need? just delete it.
+
+Worn Bathroom
+
+7 unique parts
+PBR material
+low poly models
+clear geometry
+
+Polycount:
+
+Bathtub - 1,301 polygons;
+Shower - 990 polygons;
+Sink - 1,298 polygons;
+Toilet - 907 polygons;
+Toilet Paper Holder - 254 polygons;
+Towel Rail - 864 polygons;
+Water Mixer - 714 polygons;
+
+Material:
+
+PBR material
+Albedo, Normal, Roughness.
+Unreal Engine 4 ready.
+Collision included.
+
+Thank you for purchasing this pack.
--- Assets/Scripts/Monster/MonsterAI.cs
+++ Assets/Scripts/Monster/MonsterAI.cs
@@ -1,19 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Animations;
 
 public class MonsterAI : MonoBehaviour
 {
     private NavMeshAgent AI_Monster;
     private GameObject Player;
-      
+
     public Transform[] WayPoints;
     public Transform target;
     public int Current_Patch;
 
-    public enum AI_State {Patrol, Stay, Chase};
+    public enum AI_State { Patrol, Stay, Chase };
     public AI_State AI_Enemy;
 
     private Transform Last_Point;
@@ -29,49 +27,27 @@ public class MonsterAI : MonoBehaviour
     {
         AI_Monster = GetComponent<NavMeshAgent>();
         Player = GameObject.FindGameObjectWithTag("Player");
-
     }
 
     private void FixedUpdate()
     {
+        OnFunctionCheckLastPoint();
+    }
+
+    /// <summary>
+    /// Метод вызова ходьбы по чекпоинтам для пассивного состояние
+    /// Также для обнаружения игрока и его преследования
+    /// </summary>
+    private void OnFunctionCheckLastPoint()
+    {
         if (Check_LastPoint == false)
         {
-            if (AI_Enemy == AI_State.Patrol)
-            {
-                AI_Monster.isStopped = false; //AI_Monster.Resume;
-                gameObject.GetComponent<Animator>().SetBool("isPatrolling", true);
-                AI_Monster.SetDestination(WayPoints[Current_Patch].transform.position);
-                Patch_dist = Vector3.Distance(WayPoints[Current_Patch].transform.position, gameObject.transform.position);
-                if (Patch_dist < 2)
-                {
-                    Current_Patch++;
-                    Current_Patch = Current_Patch % WayPoints.Length;
-                }
-            }
-            if (AI_Enemy == AI_State.Stay)
-            {
-                gameObject.GetComponent<Animator>().SetBool("isPatrolling", false);
-                AI_Monster.isStopped = true; //AI_Monster.Stop();
-            }
-            if (AI_Enemy == AI_State.Chase)
-            {                
-                StartCoroutine(AnimationCoroutine());
-
-                if (gameObject.GetComponent<FieldOfView>().canSeePlayer == false)
-                {
-                    Last_Point = Player.transform;
-                    Check_LastPoint = true;
-                    gameObject.GetComponent<Animator>().SetBool("isScream", false);
-                    gameObject.GetComponent<Animator>().SetBool("isChasing", false);
-
-                }
-                else
-                    AI_Monster.SetDestination(Player.transform.position);
-            }
+            OnActionPatrol();
+            OnActionChase();
         }
         else
         {
-            AI_Monster.isStopped = false; //AI_Monster.Resume();
+            AI_Monster.isStopped = false;
             i_stay += 1 * Time.deltaTime;
             PointDist = Vector3.Distance(Last_Point.transform.position, gameObject.transform.position);
             if (PointDist < 1 || i_stay >= 7)
@@ -80,33 +56,128 @@ public class MonsterAI : MonoBehaviour
                 AI_Enemy = AI_State.Patrol;
                 i_stay = 0;
             }
-            else            
-                gameObject.GetComponent<Animator>().SetBool("isPatrolling", true);
+            else
+                StartCoroutine(AnimationLookAroundAndPatrol());
+
+
         }
         DistPlayer = Vector3.Distance(Player.transform.position, gameObject.transform.position);
         if (DistPlayer < 2)
         {
             Player.SetActive(false);
-           // Panel_GameOver.SetActive(true); В случае смерти ГГ появляется меню 
+            // Panel_GameOver.SetActive(true); В случае смерти ГГ появляется меню 
+        }
+
+    }
+
+    /// <summary>
+    /// Метод пассивной ходьбы по чекпоинтам 
+    /// </summary>
+    private void OnActionPatrol()
+    {
+        if (AI_Enemy == AI_State.Patrol)
+        {
+            AI_Monster.isStopped = false;
+            gameObject.GetComponent<Animator>().SetBool("isPatrolling", true);
+            AI_Monster.SetDestination(WayPoints[Current_Patch].transform.position);
+            Patch_dist = Vector3.Distance(WayPoints[Current_Patch].transform.position, gameObject.transform.position);
+            if (Patch_dist < 2)
+            {
+                Current_Patch++;
+                Current_Patch = Current_Patch % WayPoints.Length;
+            }
         }
     }
 
-    IEnumerator AnimationCoroutine()
+    /// <summary>
+    /// Метод преследования и крика при обнаружении игрока
+    /// </summary>
+    private void OnActionChase()
     {
-        WaitWorkAnimationScream();
-        yield return new WaitForSeconds(2f);
-        WaitWorkAnimationChase();
+        if (AI_Enemy == AI_State.Chase)
+        {
+            StartCoroutine(AnimationScreamAndChase());
+        }
     }
 
+    /// <summary>
+    /// Задержка на 4 секунды для работы анимации поиска после потери игрока из виду
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AnimationLookAroundAndPatrol()
+    {
+        LookAround();
+        yield return new WaitForSeconds(4f);
+        OnPatrollingAnimation();
+    }
+
+    /// <summary>
+    /// Задержки анимации преследования на 1.5 секунды для анимации крика при обнаружении игрока
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AnimationScreamAndChase()
+    {
+        WaitWorkAnimationScream();
+        yield return new WaitForSeconds(1.5f);
+        WaitWorkAnimationChase();
+    }
+  
+    /// <summary>
+    /// Запуск анимации осмотра территории
+    /// </summary>
+    private void LookAround()
+    {
+        AI_Monster.isStopped = true;
+        gameObject.GetComponent<Animator>().SetBool("isScream", false);
+        gameObject.GetComponent<Animator>().SetBool("isLookAround", true);
+    }
+
+    /// <summary>
+    /// Запуск анимации патруля по чекпоинтам(пассивное состояние)
+    /// </summary>
+    private void OnPatrollingAnimation()
+    {
+        AI_Monster.isStopped = false;
+        gameObject.GetComponent<Animator>().SetBool("isLookAround", false);
+        gameObject.GetComponent<Animator>().SetBool("isPatrolling", true);
+    }
+
+    /// <summary>
+    /// Запуск анимации крика в сторону игрока
+    /// </summary>
     private void WaitWorkAnimationScream()
     {
         AI_Monster.isStopped = true;
         transform.LookAt(target);
-        gameObject.GetComponent<Animator>().SetBool("isScream", true);        
+        gameObject.GetComponent<Animator>().SetBool("isScream", true);
     }
+
+    /// <summary>
+    /// Запуск анимации преследования игрока
+    /// </summary>
     private void WaitWorkAnimationChase()
     {
         AI_Monster.isStopped = false;
+        AI_Monster.speed = 5f;
         gameObject.GetComponent<Animator>().SetBool("isChasing", true);
+        StateOfRest();
+    }
+
+    /// <summary>
+    /// Метод для перехода от анимации преследования к анимации патруля
+    /// </summary>
+    private void StateOfRest()
+    {
+        if (gameObject.GetComponent<FieldOfView>().canSeePlayer == false)
+        {
+            Last_Point = Player.transform;
+            Check_LastPoint = true;
+            gameObject.GetComponent<Animator>().SetBool("isScream", false);
+            gameObject.GetComponent<Animator>().SetBool("isChasing", false);
+            AI_Monster.speed = 1f;
+            AI_Monster.isStopped = false;
+        }
+        else
+            AI_Monster.SetDestination(Player.transform.position);
     }
 }
